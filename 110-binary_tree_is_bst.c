33763ec3b0266bfd38f3f1804d4dc789c0fe2cea
jhay <55604668+jhay-smart@users.noreply.github.com>
2023-10-05 12:58:28 +0100

    Update 110-binary_tree_is_bst.c

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,40 +1,37 @@
 #include "binary_trees.h"
-#include <limits.h>
 
 /**
- * binary_tree_is_bst - subfunction that returns true if the
- * given tree is a BST and its values are >= min and <= max.
- * @tree: pointer to the root node of the tree to check
- * @min: INT_MIN
- * @max: INT_MAX
- * Return: If tree is NULL, your function must return 0
- */
-int binary_tree_is_bst(const binary_tree_t *tree, int min, int max)
-{
-	/* an empty tree is BST */
-	if (tree == NULL)
-		return (1);
-
-	/* false if this tree violates the min/max constraint */
-	if (tree->n < min || tree->n > max)
-		return (0);
-
-	/* otherwise check the subtrees recursively, tightening*/
-	/* the min or max constraint */
-	return (binary_tree_is_bst(tree->left, min, tree->n - 1) &&
-	binary_tree_is_bst(tree->right, tree->n + 1, max));
-	/* Allow only distinct values */
-}
-
-/**
- * binary_tree_is_bst - function that checks if a binary tree
- * is a valid Binary Search Tree
- * @tree: pointer to the root node of the tree to check
- * Return: If tree is NULL, your function must return 0
+ * binary_tree_is_bst - checks if a binary tree is a valid Binary Search Tree
+ * @tree: a pointer to the root node of the tree to check
+ *
+ * Return: 1 if tree is a valid BST
+ *         0 otherwise
  */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
-	if (tree == NULL)
+	if (!tree)
 		return (0);
-	return (binary_tree_is_bst(tree, INT_MIN, INT_MAX));
+	return (btib_helper(tree, INT_MIN, INT_MAX));
+}
+
+/**
+ * btib_helper - checks if a binary tree is a valid Binary Search Tree
+ * @tree: a pointer to the root node of the tree to check
+ * @min: Lower bound of checked nored
+ * @max: Upper bound of checked nodes
+ *
+ * Return: 1 if tree is a valid BST
+ *         0 otherwise
+ */
+int btib_helper(const binary_tree_t *tree, int min, int max)
+{
+	if (!tree)
+		return (1);
+
+	if (tree->n < min || tree->n > max)
+		return (0);
+
+	return (btib_helper(tree->left, min, tree->n - 1) &&
+		btib_helper(tree->right, tree->n + 1, max));
+	/* -1 and +1 stem from "There must be no duplicate values" req */
 }
