096b1641c4bbeda368ae42095bc1b6c1dbbc8651
jhay <55604668+jhay-smart@users.noreply.github.com>
2023-10-05 13:38:46 +0100

    Update 134-heap_to_sorted_array.c

--- 134-heap_to_sorted_array.c
+++ 134-heap_to_sorted_array.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+#include <stdlib.h>
 
 /**
  * heap_to_sorted_array - converts a Binary Max Heap
@@ -11,5 +12,23 @@
  */
 int *heap_to_sorted_array(heap_t *heap, size_t *size)
 {
+    int *sorted_array;
+    size_t i;
 
+    if (!heap || !size)
+        return (NULL);
+
+    *size = binary_tree_size(heap);
+    sorted_array = malloc(sizeof(int) * (*size));
+
+    if (!sorted_array)
+        return (NULL);
+
+    for (i = 0; i < *size; i++)
+    {
+        sorted_array[i] = heap->n;
+        heap_extract(&heap);
+    }
+
+    return (sorted_array);
 }
